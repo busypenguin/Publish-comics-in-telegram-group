446ccfb1b629826b8b747d61e387f6f2cd575a28
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-07-09 16:43:32 +0300

    change function download_image()

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def download_image(url, folder_path):
     filename = url.split("/")[-1]
     response = requests.get(url)
     response.raise_for_status()
-    os.makedirs(folder_path, exist_ok=True)
     filepath = f'{folder_path}{filename}'
     with open(filepath, "wb") as file:
         file.write(response.content)
@@ -29,6 +28,8 @@ env.read_env()
 telegram_bot_token = env.str('TELEGRAM_BOT_TOKEN')
 tg_chat_id = env.str('TG_CHAT_ID')
 bot = telegram.Bot(token=telegram_bot_token)
+
+os.makedirs('images/', exist_ok=True)
 
 filepath = download_image(img_url, 'images/')
 with open(filepath, 'rb') as photo:
